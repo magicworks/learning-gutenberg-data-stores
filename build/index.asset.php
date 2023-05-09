fafea3d9f40019d535ab73885e698a685c1b3b86
Laura Lieknina <laura@magicworks.lv>
2023-05-09 19:38:16 +0300

    move custom data store that gets data from API together with the block that uses it

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b49bbe708589d55549bcd4037d4cdeac');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '3f993c458586095ac1cc06282d0b5b66');
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import { registerBlockType } from '@wordpress/blocks';
 import Edit from './edit';
 import save from './save';
 
+import './todos-store';
+
 registerBlockType('learning-gutenberg/data-stores', {
 	edit: Edit,
 	save,
