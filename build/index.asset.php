bc3bd7f8019ccdfa824b46580616fdc32b86014d
Laura Lieknina <laura@magicworks.lv>
2023-05-09 20:01:32 +0300

    add new panel to default post sidebar

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'ac480ee43124e256c2602f1aff49584a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'c65a9be0cac856fd7917adea9d74e2b2');
--- src/plugins/sidebar.js
+++ src/plugins/sidebar.js
@@ -1,17 +1,28 @@
 import { registerPlugin } from '@wordpress/plugins';
-import { PluginSidebar } from '@wordpress/edit-post';
+import {
+	PluginSidebar,
+	PluginDocumentSettingPanel,
+} from '@wordpress/edit-post';
 import { __ } from '@wordpress/i18n';
 
 registerPlugin('learning-gutenberg-plugin', {
 	render: () => {
 		return (
-			<PluginSidebar
-				name="meta-fields-sidebar"
-				icon="admin-settings"
-				title={__('Post Options', 'learning-gutenberg-data-stores')}
-			>
-				Custom Sidebar
-			</PluginSidebar>
+			<>
+				<PluginDocumentSettingPanel
+					title="My Panel"
+					icon="admin-settings"
+				>
+					<p>Document Settings Panel</p>
+				</PluginDocumentSettingPanel>
+				<PluginSidebar
+					name="meta-fields-sidebar"
+					icon="admin-settings"
+					title={__('Post Options', 'learning-gutenberg-data-stores')}
+				>
+					Custom Sidebar
+				</PluginSidebar>
+			</>
 		);
 	},
 });
